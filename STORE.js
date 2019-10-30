50a2531f6e7c1a7119b4fd2f5298e99979f800a9
Catherine Davis <catherinevtdavis@gmail.com>
2019-10-30 10:28:57 -0600

    Finished questions

--- STORE.js
+++ STORE.js
@@ -31,6 +31,73 @@ const STORE = [
         correctAnswer: 'Bonjour!'
     },
     {
-
+        question: 'What is the word for kiss?',
+        answers: [
+            'Chanter',
+            'Ecouter',
+            'Bisou',
+            'Manger'
+        ],
+        correctAnswer: 'Bisou'
+    },
+    {
+        question: 'The word \xE9toile means:',
+        answers: [
+            'Whistle',
+            'Sing',
+            'Star',
+            'Love'
+        ],
+        correctAnswer: 'Star'
+    },
+    {
+        question: 'What is the word for butterfly?',
+        answers: [
+            'La Butterfrie',
+            'Le Papillon',
+            'Le Chat',
+            'Doux'
+        ],
+        correctAnswer: 'Le Papillon'
+    },
+    {
+        question: '"J\'aime ma soeur" means:',
+        answers: [
+            'I like soccer',
+            'I like my sister',
+            'I aim to please',
+            'Jerry aims my sword'
+        ],
+        correctAnswer: 'I like my sister'
+    },
+    {
+        question: 'The word soleil means:',
+        answers: [
+            'Solar power',
+            'Sun',
+            'Sorry',
+            'Sold'
+        ],
+        correctAnswer: 'Sun'
+    },
+    {
+        question: 'What is the capital of France?',
+        answers: [
+            'Washington',
+            'Paris',
+            'Versailles',
+            'Chanter'
+        ],
+        correctAnswer: 'Paris'
+    },
+    {
+        question: 'What is the word for grapefruit?',
+        answers: [
+            'Le pamplemousse',
+            'Le lionfruit',
+            'Le camembert',
+            'Le citronnade'
+        ],
+        correctAnswer: 'Le pamplemousse'
     }
 ];
