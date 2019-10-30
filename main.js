2a04b42082e9b8e61d81d28c0f291e1cffa3dd91
Catherine Davis <catherinevtdavis@gmail.com>
2019-10-29 20:06:28 -0600

    Added pseudocode

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Start button toggle classes
+//Start Quiz
 function startQuiz() {
     $('#start-quiz').on("click", function (event) {
         $(".start-quiz").toggleClass('hidden');
@@ -6,5 +6,23 @@ function startQuiz() {
         console.log("start button pressed");
     });
 }
-startQuiz();
+//Reveal quiz question
+function generateQuestion(){}
+//Submit and check answer
+function submitAnswer(){}
+//Display good or bad window
+function displayCorrectOrNot(){}
+//Update question number and score
+function updateQuestionAndScore(){}
+//Render next question
+function nextQuestion(){}
+//Calculate and show results
+function finalResult(){}
+//Restart quiz
+function restartQuiz(){}
+//Run all functions
+function runQuiz(){
+    startQuiz();
+}
 
+$(runQuiz);
