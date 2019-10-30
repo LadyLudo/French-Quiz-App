6fd0489f8d65aa18a7b2e4cc7ed3b679db3d5c75
Catherine Davis <catherinevtdavis@gmail.com>
2019-10-29 20:15:04 -0600

    Added questions

--- STORE.js
+++ STORE.js
@@ -1,1 +1,36 @@
-//Questions and Answers will be stored here
+//Questions and Answers
+const STORE = [
+    {
+        question: 'What is the word for cheese?',
+        answers: [
+            'Le fromage',
+            'Le pain',
+            'Le chocolat',
+            'Le buerre'
+        ],
+        correctAnswer: 'Le fromage'
+    },
+    {
+        question: 'What is the word for milk?',
+        answers: [
+            'Le buerre',
+            'Le pain',
+            'Le fromage',
+            'Le lait'
+        ],
+        correctAnswer: 'Le lait'
+    },
+    {
+        question: 'How do you say hello?',
+        answers: [
+            'Bonjour!',
+            'Au Revoir!',
+            'Bonne Nuit',
+            'Je m\'appelle'
+        ],
+        correctAnswer: 'Bonjour!'
+    },
+    {
+
+    }
+]
